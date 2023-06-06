95feac14edc821c4c4bb70e2027282c65a8c5bee
Svezi, Jan <jan.svezi@oracle.com>
2023-06-06 09:55:27 +0200

    Data from API are rendered via REST Data Provider and `oj-table` component.

---
+++ src/components/content/pages/characters/characters-types.ts
@@ -0,0 +1,8 @@
+export type CharacterAPIType = {
+    Name: string;
+    Gender: string;
+    Homeworld: string;
+    Born: string;
+    Jedi: string;
+    Created: string;
+};
--- src/components/content/pages/characters/characters.tsx
+++ src/components/content/pages/characters/characters.tsx
@@ -1,3 +1,58 @@
+import { ComponentProps } from "preact";
+import { useMemo } from "preact/hooks";
+
+import "ojs/ojtable";
+
+import { CharacterAPIType } from "./characters-types";
+
+import { RESTDataProvider } from "ojs/ojrestdataprovider";
+
+type TableProps = ComponentProps<"oj-table">;
+
+const setColumnsDefault: TableProps["columnsDefault"] = { sortable: "disabled" };
+const columns: TableProps["columns"] = [
+    { headerText: "Name", field: "Name" },
+    { headerText: "Gender", field: "Gender" },
+    { headerText: "Homeworld", field: "Homeworld" },
+    { headerText: "Born", field: "Born" },
+    { headerText: "Is jedi?", field: "Jedi" },
+    { headerText: "Created", field: "Created" }
+];
+
+const API_ENDPOINT: Readonly<string> = "http://127.0.0.1:3333/star-wars";
+
 export default function Characters() {
-    return <div>Postavy</div>;
+    const restDataProvider: RESTDataProvider<CharacterAPIType["Name"], CharacterAPIType> = useMemo(() => new RESTDataProvider({
+        keyAttributes: "Name",
+        url: API_ENDPOINT,
+        transforms: {
+            fetchFirst: {
+                request: async (options) =>  {
+                    const url = new URL(options.url);
+
+                    const { size, offset } = options.fetchParameters;
+                    url.searchParams.set("limit", String(size));
+                    url.searchParams.set("offset", String(offset));
+
+                    return new Request(url.href);
+                },
+                response: async ({ body }) => {
+                    const { totalSize, hasMore, data } = body;
+                    return { data, totalSize, hasMore };
+                }
+            }
+        }
+    }), []);
+
+    return (
+        <div className="oj-flex">
+            <oj-table
+                aria-label="Alerts"
+                data={restDataProvider}
+                columnsDefault={setColumnsDefault}
+                columns={columns}
+                class="oj-bg-body table-sizing oj-flex-item full-height">
+            </oj-table>
+        </div>
+    );
 }
