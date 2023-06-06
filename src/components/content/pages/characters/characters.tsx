b30c148805c314bb7b646386ad474ca7e6215f6c
Svezi, Jan <jan.svezi@oracle.com>
2023-06-06 11:19:33 +0200

    Implemented handling of action events in characters table.

--- src/components/content/pages/characters/characters.tsx
+++ src/components/content/pages/characters/characters.tsx
@@ -2,8 +2,10 @@ import { ComponentProps } from "preact";
 import { useMemo, useCallback } from "preact/hooks";
 
 import { ojTable } from "ojs/ojtable";
+import { ojButton } from "ojs/ojbutton";
 
 import "ojs/ojtable";
+import "ojs/ojbutton";
 
 import { CharacterAPIType, CharacterType } from "./characters-types";
 
@@ -12,6 +14,8 @@ import { RESTDataProvider } from "ojs/ojrestdataprovider";
 type TableProps = ComponentProps<"oj-table">;
 
 const setColumnsDefault: TableProps["columnsDefault"] = { sortable: "disabled" };
+const setSelectionMode: TableProps["selectionMode"] = { row: "single", column: "none" };
+
 const columns: TableProps["columns"] = [
     { headerText: "Greeting", field: "Greeting" },
     { headerText: "Name", field: "Name" },
@@ -23,7 +27,8 @@ const columns: TableProps["columns"] = [
         return {
             insert: cell.row.Created.toLocaleDateString()
         };
-    } }
+    } },
+    { headerText: "", template: "actionTemplate" }
 ];
 
 const API_ENDPOINT: Readonly<string> = "http://localhost:3333/star-wars";
@@ -64,8 +69,34 @@ export default function Characters() {
     }), []);
 
     const renderGenderColumn = useCallback((cell: ojTable.CellTemplateContext<CharacterType["Name"], CharacterType>) =>
-        cell.row.Gender === "female" ? <span className="oj-ux-ico-female" /> : <span className="oj-ux-ico-male" />,
+            cell.row.Gender === "female" ? <span className="oj-ux-ico-female" /> : <span className="oj-ux-ico-male" />,
         []);
+
+    const renderActionColumn = useCallback((cell: ojTable.CellTemplateContext<CharacterType["Name"], CharacterType>) => {
+        const handleEdit = (event: ojButton.ojAction) => {
+            event.detail.originalEvent.stopPropagation();
+            console.log('edit', cell.row);
+        };
+
+        const handleDelete = (event: ojButton.ojAction) => {
+            event.detail.originalEvent.stopPropagation();
+            console.log('delete', cell.row);
+        };
+
+        return [
+            <oj-button class="oj-button-sm" onojAction={handleEdit} display="icons">
+                <span slot="startIcon" className="oj-ux-ico-edit"></span> Edit
+            </oj-button>,
+
+            <oj-button class="oj-button-sm" onojAction={handleDelete} display="icons">
+                <span slot="startIcon" className="oj-ux-ico-delete-all"></span> Delete
+            </oj-button>
+        ];
+    }, []);
+
+    const handleCharacterChanged = useCallback((event: ojTable.firstSelectedRowChanged<CharacterType["Name"], CharacterType>) => {
+        console.log('show', event.detail.value.data);
+    }, []);
 
     return (
         <div className="oj-flex">
@@ -73,9 +104,12 @@ export default function Characters() {
                 aria-label="Alerts"
                 data={restDataProvider}
                 columnsDefault={setColumnsDefault}
+                selectionMode={setSelectionMode}
                 columns={columns}
+                onfirstSelectedRowChanged={handleCharacterChanged}
                 class="oj-bg-body table-sizing oj-flex-item full-height">
                 <template slot="genderTemplate" render={renderGenderColumn} />
+                <template slot="actionTemplate" render={renderActionColumn} />
             </oj-table>
         </div>
     );
